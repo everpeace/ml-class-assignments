9bc0ccea17223e1a081e346d7494074177096c9f
Shingo Omura <everpeace@gmail.com>
2011-10-23 15:30:42 +0900

    assignments of regularization are done and submitted.

--- ex2/mlclass-ex2/costFunctionReg.m
+++ ex2/mlclass-ex2/costFunctionReg.m
@@ -2,12 +2,12 @@ function [J, grad] = costFunctionReg(theta, X, y, lambda)
 %COSTFUNCTIONREG Compute cost and gradient for logistic regression with regularization
 %   J = COSTFUNCTIONREG(theta, X, y, lambda) computes the cost of using
 %   theta as the parameter for regularized logistic regression and the
-%   gradient of the cost w.r.t. to the parameters. 
+%   gradient of the cost w.r.t. to the parameters.
 
 % Initialize some useful values
 m = length(y); % number of training examples
 
-% You need to return the following variables correctly 
+% You need to return the following variables correctly
 J = 0;
 grad = zeros(size(theta));
 
@@ -17,10 +17,21 @@ grad = zeros(size(theta));
 %               Compute the partial derivatives and set grad to the partial
 %               derivatives of the cost w.r.t. each parameter in theta
 
+% calculate cost function
+h = sigmoid(X*theta);
+% calculate penalty
+% excluded the first theta value
+theta1 = theta(2:size(theta), :);
+p = lambda*(theta1'*theta1)/(2*m);
+J = ((-y)'*log(h) - (1-y)'*log(1-h))/m + p;
 
-
-
-
+% calculate grads
+for j = 1:size(theta)
+  if(j == 1)
+    grad(j) = ((h - y)'*X(:, j))/m;
+  else
+    grad(j) = ((h - y)'*X(:, j) + lambda*theta(j))/m;
+end
 
 % =============================================================
 
