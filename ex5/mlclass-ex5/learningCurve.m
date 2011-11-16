8114c18fa38c531897a03c0b40250c1ea078830f
Shingo Omura <everpeace@gmail.com>
2011-11-16 15:30:28 +0900

    ex5 "Regularized Linear Regression and Bias v.s. Variances" is done and submitted.

--- ex5/mlclass-ex5/learningCurve.m
+++ ex5/mlclass-ex5/learningCurve.m
@@ -53,16 +53,13 @@ error_val   = zeros(m, 1);
 %       end
 %
 
-% ---------------------- Sample Solution ----------------------
+for i= 1:m
+    theta          = trainLinearReg(X(1:i,:), y(1:i), lambda);
+    error_train(i) = linearRegCostFunction(X(1:i,:)   , y(1:i)   , theta, 0);
+    error_val(i)   = linearRegCostFunction(Xval, yval, theta, 0);
+end
 
-
-
-
-
-
-
-% -------------------------------------------------------------
-
+%
 % =========================================================================
 
 end
